b4a007bd6aa9846dc783c237948ddd7f2400ec00
Ryan Freckleton <ryan.freckleton@gmail.com>
2015-11-24 14:44:34 -0700

    Adding doit

--- README.markdown
+++ README.markdown
@@ -2,7 +2,7 @@
 
 - ~~Hello World~~
 - ~~[pep8](https://pypi.python.org/pypi/pep8)~~
-- [doit](http://pydoit.org/)
+- ~~[doit](http://pydoit.org/)~~
 - [pylint](https://pypi.python.org/pypi/pylint/1.4.4)
 - [py.test](http://pytest.org/latest/)
 - Deployment?
---
+++ dodo.py
@@ -0,0 +1,31 @@
+import os
+import glob
+
+from pyflakes.scripts.pyflakes import checkPath
+
+
+DOIT_CONFIG = {
+    # output from actions should be sent to the terminal/console
+    'verbosity': 2,
+    # does not stop execution on first task failure
+    'continue': True,
+    # doit itself should not produce any output (use only actions output)
+    'reporter': 'zero',
+    # use multi-processing / parallel execution
+    'num_process': 2,
+    }
+
+
+def check_path(filename):
+    """execute pyflakes checker"""
+    return not bool(checkPath(filename))
+
+def task_pyflakes():
+    """generate task for each file to be checked"""
+    for filename in glob.glob('*.py'):
+        path = os.path.abspath(filename)
+        yield {
+            'name': path, # name required to identify tasks
+            'file_dep': [path], # file dependency
+            'actions': [(check_path, (filename,)) ],
+            }
