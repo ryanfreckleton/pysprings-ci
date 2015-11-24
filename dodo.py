7a747affa3ee8135c473d49e7a83ac5221a4a672
Ryan Freckleton <ryan.freckleton@gmail.com>
2015-11-24 14:55:24 -0700

    Fixing more pep8 errors

--- dodo.py
+++ dodo.py
@@ -20,12 +20,13 @@ def check_path(filename):
     """execute pyflakes checker"""
     return not bool(checkPath(filename))
 
+
 def task_pyflakes():
     """generate task for each file to be checked"""
     for filename in glob.glob('*.py'):
         path = os.path.abspath(filename)
         yield {
-            'name': path, # name required to identify tasks
-            'file_dep': [path], # file dependency
-            'actions': [(check_path, (filename,)) ],
+            'name': path,  # name required to identify tasks
+            'file_dep': [path],  # file dependency
+            'actions': [(check_path, (filename,))],
             }
